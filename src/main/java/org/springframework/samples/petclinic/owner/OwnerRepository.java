f08d342dd7ba97456366e768fb09b070ba9d67e0
Arthur <arthur.garmider@gmail.com>
2022-04-07 13:41:04 +0300

    add imports

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -23,6 +23,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import com.exacttarget.fuelsdk;
+import com.mixpanel.mixpanelapi.MixpanelAPI;
 
 /**
  * Repository class for <code>Owner</code> domain objects All method names are compliant
